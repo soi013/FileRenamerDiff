652dfc2c228a523f2211786bdcad53b8fe885e1d
soi013 <soi2flatline@gmail.com>
2020-05-06 16:58:10 +0900

    多言語対応

--- src/FileRenamerDiff/Properties/Resources.Designer.cs
+++ src/FileRenamerDiff/Properties/Resources.Designer.cs
@@ -61,6 +61,78 @@ namespace FileRenamerDiff.Properties {
         }
         
         /// <summary>
+        ///   Changed に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_Changed {
+            get {
+                return ResourceManager.GetString("Grid_Changed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Duplicated に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_Conflicted {
+            get {
+                return ResourceManager.GetString("Grid_Conflicted", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Created Date に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_CreatedDate {
+            get {
+                return ResourceManager.GetString("Grid_CreatedDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Date modified に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_DateModified {
+            get {
+                return ResourceManager.GetString("Grid_DateModified", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Directory に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_Directory {
+            get {
+                return ResourceManager.GetString("Grid_Directory", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   NEW File Name に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_NewText {
+            get {
+                return ResourceManager.GetString("Grid_NewText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   OLD File Name に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_OldText {
+            get {
+                return ResourceManager.GetString("Grid_OldText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Size に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Grid_Size {
+            get {
+                return ResourceManager.GetString("Grid_Size", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   ## 3rd Party Libraries / Icons
         ///
         ///Following codes / libraries make it happen, thanks for their work.
@@ -92,6 +164,160 @@ namespace FileRenamerDiff.Properties {
         public static string License {
             get {
                 return ResourceManager.GetString("License", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   DRY RUN に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Menu_DryRun {
+            get {
+                return ResourceManager.GetString("Menu_DryRun", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   SAVE に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Menu_Save {
+            get {
+                return ResourceManager.GetString("Menu_Save", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Search Files に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Menu_SearchFiles {
+            get {
+                return ResourceManager.GetString("Menu_SearchFiles", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   ADD に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_Add {
+            get {
+                return ResourceManager.GetString("Setting_Add", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   As
+        ///REGEXP に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_AsExpression {
+            get {
+                return ResourceManager.GetString("Setting_AsExpression", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Delete Texts に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_DeleteTexts {
+            get {
+                return ResourceManager.GetString("Setting_DeleteTexts", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Text に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_DeleteTextTarget {
+            get {
+                return ResourceManager.GetString("Setting_DeleteTextTarget", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   File Search に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_FileSearch {
+            get {
+                return ResourceManager.GetString("Setting_FileSearch", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Ignore Directory に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_IgnoreDirectory {
+            get {
+                return ResourceManager.GetString("Setting_IgnoreDirectory", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Ignore Extensions に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_IgnoreExtensions {
+            get {
+                return ResourceManager.GetString("Setting_IgnoreExtensions", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Other に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_Other {
+            get {
+                return ResourceManager.GetString("Setting_Other", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Replace Patterns に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_ReplacePatterns {
+            get {
+                return ResourceManager.GetString("Setting_ReplacePatterns", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Replace Text に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_ReplaceText {
+            get {
+                return ResourceManager.GetString("Setting_ReplaceText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Target Text に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_ReplaceTextTarget {
+            get {
+                return ResourceManager.GetString("Setting_ReplaceTextTarget", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Reset ALL Settings に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_ResetAllSetting {
+            get {
+                return ResourceManager.GetString("Setting_ResetAllSetting", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Search Sub Directories に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_SearchSubDirectories {
+            get {
+                return ResourceManager.GetString("Setting_SearchSubDirectories", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Settings に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string Setting_SettingTab {
+            get {
+                return ResourceManager.GetString("Setting_SettingTab", resourceCulture);
             }
         }
     }
