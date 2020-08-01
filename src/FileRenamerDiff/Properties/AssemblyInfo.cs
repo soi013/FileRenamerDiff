71a59e9ef602a81ce5f9b91d17aea729c067b52d
soi013 <soi2flatline@gmail.com>
2020-08-01 11:16:27 +0900

    ドラッグできるDataGridに、ドラッグできることを表示するIconを追加

---
+++ src/FileRenamerDiff/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Runtime.InteropServices;
+
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+
+// このような SDK スタイルのプロジェクトの場合、以前はこのファイルで定義していたいくつかのアセンブリ属性がビルド時に自動的に追加されて、プロジェクトのプロパティで定義されている値がそれに設定されるようになりました。組み込まれる属性と、このプロセスをカスタマイズする方法の詳細については、次を参照してください:
+// https://aka.ms/assembly-info-properties
+
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから参照できなくなります。このアセンブリ内の型に COM からアクセスする必要がある場合は、その型の
+// ComVisible 属性を true に設定してください。
+
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります。
+
+[assembly: Guid("a81e68cb-2834-41e9-96dc-4220efbce584")]
---
+++ src/FileRenamerDiff/ViewsAsset/DataGridIconColumn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace FileRenamerDiff.Views
+{
+    /// <summary>
+    /// PackIconを表示するDataGrid列
+    /// </summary>
+    public class DataGridIconColumn : DataGridTemplateColumn
+    {
+        #region PackIcon依存関係プロパティ
+        public PackIconKind Kind
+        {
+            get => (PackIconKind)GetValue(PackIconProperty);
+            set => SetValue(PackIconProperty, value);
+        }
+        public static readonly DependencyProperty PackIconProperty =
+            DependencyProperty.Register(nameof(Kind), typeof(PackIconKind), typeof(DataGridIconColumn), new PropertyMetadata(default(PackIconKind), propertyChanged));
+
+        private static void propertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is PackIconKind iconKind
+                && d is DataGridIconColumn iconColumn)
+            {
+                var iconFactory = new FrameworkElementFactory(typeof(PackIcon));
+                iconFactory.SetValue(PackIcon.KindProperty, iconKind);
+                iconColumn.CellTemplate = new DataTemplate() { VisualTree = iconFactory };
+            }
+        }
+        #endregion
+
+        public DataGridIconColumn()
+        {
+            //選択もタブストップもできないセルにする
+            if (App.Current.TryFindResource("IgnoreCell") is Style style)
+            {
+                this.CellStyle = style;
+            }
+        }
+    }
+}
