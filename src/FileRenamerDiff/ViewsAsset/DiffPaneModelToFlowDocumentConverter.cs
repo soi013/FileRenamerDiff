550a1b14234a41169e007e25d21eaaf1b43fcc04
soi013 <soi2flatline@gmail.com>
2021-08-10 22:46:09 +0900

    CultureDisplayConverterとDiffPaneModelToFlowDocumentConverterのテストを追加

--- src/FileRenamerDiff/ViewsAsset/DiffPaneModelToFlowDocumentConverter.cs
+++ src/FileRenamerDiff/ViewsAsset/DiffPaneModelToFlowDocumentConverter.cs
@@ -16,15 +16,22 @@ using FileRenamerDiff.Models;
 
 namespace FileRenamerDiff.Views
 {
+    [ValueConversion(typeof(DiffPaneModel), typeof(FlowDocument))]
     public class DiffPaneModelToFlowDocumentConverter : IValueConverter
     {
-        private static readonly Brush unchangeBrush = Colors.Transparent.ToSolidColorBrush(true);
-        private static readonly Brush deletedBrush = AppExtention.ToColorOrDefault($"#FFAFD1").ToSolidColorBrush(true);
-        private static readonly Brush insertedBrush = AppExtention.ToColorOrDefault($"#88E6A7").ToSolidColorBrush(true);
-        private static readonly Brush imaginaryBrush = Colors.SkyBlue.ToSolidColorBrush(true);
-        private static readonly Brush modifiedBrush = Colors.Orange.ToSolidColorBrush(true);
+        internal const string UnchangeColorCode = "Transparent";
+        internal const string DeletedColorCode = "#FFAFD1";
+        internal const string InsertedColorCode = "#88E6A7";
+        internal const string ImaginaryColorCode = "SkyBlue";
+        internal const string ModifiedColorCode = "Orange";
+        private static readonly Brush unchangeBrush = AppExtention.ToColorOrDefault(UnchangeColorCode).ToSolidColorBrush(true);
+        private static readonly Brush deletedBrush = AppExtention.ToColorOrDefault(DeletedColorCode).ToSolidColorBrush(true);
+        private static readonly Brush insertedBrush = AppExtention.ToColorOrDefault(InsertedColorCode).ToSolidColorBrush(true);
+        private static readonly Brush imaginaryBrush = AppExtention.ToColorOrDefault(ImaginaryColorCode).ToSolidColorBrush(true);
+        private static readonly Brush modifiedBrush = AppExtention.ToColorOrDefault(ModifiedColorCode).ToSolidColorBrush(true);
         private static readonly Brush changedTextBrush = Colors.Black.ToSolidColorBrush(true);
-        private static readonly Brush normalTextBrush = (SolidColorBrush)App.Current.Resources["MaterialDesignBody"];
+        private static readonly Brush normalTextBrush = (Application.Current?.Resources["MaterialDesignBody"] as SolidColorBrush)
+            ?? Colors.Black.ToSolidColorBrush(true);
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
@@ -37,6 +44,11 @@ namespace FileRenamerDiff.Views
             if (diffVM.Lines.Count > 1)
                 LogTo.Warning("Lines Count is over. {@LinesCount}", diffVM.Lines.Count);
 
+            return ConvertCore(diffVM);
+        }
+
+        private static FlowDocument ConvertCore(DiffPaneModel diffVM)
+        {
             List<Run> lineView = ConvertLinveVmToRuns(diffVM.Lines.First());
 
             var paragraph = new Paragraph();
@@ -50,6 +62,7 @@ namespace FileRenamerDiff.Views
                 //ChangeType.Modifiedだったら変更された部分だけハイライトしたいのでSubPieceからいろいろやる
                 ChangeType.Modified => lineVM
                     .SubPieces
+                    .Where(x => x.Text is not null)
                     .Select(x => ConvertPieceVmToRun(x))
                     .ToList(),
 
---
+++ src/UnitTests/Test_Converters.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+using Anotar.Serilog;
+
+using DiffPlex.DiffBuilder.Model;
+
+using FileRenamerDiff.Models;
+using FileRenamerDiff.ViewModels;
+using FileRenamerDiff.Views;
+
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using FluentAssertions.Primitives;
+
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using Reactive.Bindings.Schedulers;
+
+using Xunit;
+
+namespace UnitTests
+{
+    public class Test_Converters : IClassFixture<LogFixture>
+    {
+        [WpfFact]
+        public void Test_CultureDisplayConverter()
+        {
+            CultureDisplayConverter converter = new();
+            converter.Convert(CultureInfo.InvariantCulture, 0, CultureInfo.InvariantCulture)
+                .Should().Contain("Auto");
+
+            var englishCulture = CultureInfo.GetCultureInfo("en");
+
+            Thread.CurrentThread.CurrentUICulture = englishCulture;
+
+            converter.Convert(CultureInfo.GetCultureInfo("en"), 0, englishCulture)
+                .Should().ContainAll("en", "English");
+            converter.Convert(CultureInfo.GetCultureInfo("ru"), 0, englishCulture)
+                .Should().ContainAll("ru", "русский");
+            converter.Convert(CultureInfo.GetCultureInfo("ja"), 0, englishCulture)
+                .Should().ContainAll("ja", "日本語");
+            converter.Convert(CultureInfo.GetCultureInfo("zh"), 0, englishCulture)
+                .Should().ContainAll("zh", "中文");
+            converter.Convert(CultureInfo.GetCultureInfo("de"), 0, englishCulture)
+                .Should().ContainAll("de", "Deutsch");
+        }
+
+        private static readonly Color UnchangeC = AppExtention.ToColorOrDefault(DiffPaneModelToFlowDocumentConverter.UnchangeColorCode);
+        private static readonly Color DeletedC = AppExtention.ToColorOrDefault(DiffPaneModelToFlowDocumentConverter.DeletedColorCode);
+        private static readonly Color InsertedC = AppExtention.ToColorOrDefault(DiffPaneModelToFlowDocumentConverter.InsertedColorCode);
+        private static readonly Color ImaginaryC = AppExtention.ToColorOrDefault(DiffPaneModelToFlowDocumentConverter.ImaginaryColorCode);
+
+        [WpfFact]
+        public void DiffPaneMToFlowDocConverter()
+        {
+            LogTo.Information("Start");
+
+            DiffPaneModelToFlowDocumentConverter converter = new();
+
+            var testCases = new (string oldFileName, string newFileName, Color[] oldColors, Color[] newColors)[]
+            {
+                ("abc.txt", "abc.txt", new[]{ UnchangeC }, new[]{ UnchangeC }),
+                ("abc.txt", "def.txt", new[]{ DeletedC, UnchangeC, UnchangeC }, new[]{ InsertedC, UnchangeC, UnchangeC }),
+                ("abc.txt", "azc.txt", new[]{ DeletedC, UnchangeC, UnchangeC }, new[]{ InsertedC, UnchangeC, UnchangeC }),
+                ("fix tgt_fix", "fix REP_fix", new[]{ UnchangeC, UnchangeC, DeletedC, UnchangeC, UnchangeC }, new[]{ UnchangeC, UnchangeC, InsertedC, UnchangeC, UnchangeC }),
+                ("Del.ete.txt", "Del..txt", new[]{ UnchangeC, DeletedC, DeletedC, DeletedC, UnchangeC }, new[]{ UnchangeC,  InsertedC,  UnchangeC }),
+            };
+
+            foreach (var (oldFileName, newFileName, oldColors, newColors) in testCases)
+            {
+                SideBySideDiffModel diffModel = AppExtention.CreateDiff(oldFileName, newFileName);
+
+                //inlineDataの配列は1つしか受け取れない制限のため、同じ組み合わせでoldとnewを別テストメソッドで行う
+                DiffPaneMSide(converter, diffModel.OldText, oldColors);
+                DiffPaneMSide(converter, diffModel.NewText, newColors);
+            }
+        }
+
+        private static void DiffPaneMSide(DiffPaneModelToFlowDocumentConverter converter, DiffPaneModel diffPaneModel, IEnumerable<Color> expectedColors)
+        {
+            var flowDoc = (FlowDocument)converter.Convert(diffPaneModel, typeof(DiffPaneModel), 0, CultureInfo.InvariantCulture);
+
+            IEnumerable<Color> actualBrushes = ((Paragraph)flowDoc.Blocks.FirstBlock).Inlines
+                                .Select(x => (SolidColorBrush)x.Background)
+                                .Select(x => x.Color);
+            actualBrushes
+                .Should().BeEquivalentTo(expectedColors, "背景色が差分に合わせた色で区切られているはず");
+        }
+    }
+}
