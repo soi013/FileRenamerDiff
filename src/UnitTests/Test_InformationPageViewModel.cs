e3bbc882d977520378dbb7f198e4c6b72b003cfa
soi013 <soi2flatline@gmail.com>
2021-08-20 22:54:31 +0900

    アプリケーション情報ダイアログVMのテストを追加

---
+++ src/UnitTests/Test_InformationPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using FileRenamerDiff.Models;
+using FileRenamerDiff.ViewModels;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace UnitTests
+{
+    public class Test_InformationPageViewModel
+    {
+        [Fact]
+        public void Test_InformationPageViewModel_Create()
+        {
+            InformationPageViewModel.AppInfoText
+                .Should().ContainAll("File Renamer Diff", "soi013");
+        }
+    }
+}
