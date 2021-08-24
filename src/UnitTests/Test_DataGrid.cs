7e6c470805fcece09514d1b33db9deb032602c31
soi013 <soi2flatline@gmail.com>
2021-08-24 21:15:33 +0900

    Bindingしない直接指定のテストも追加

--- src/UnitTests/Test_DataGrid.cs
+++ src/UnitTests/Test_DataGrid.cs
@@ -50,38 +50,91 @@ namespace UnitTests
         }
 
         [WpfFact]
-        public void Test_DataGridOperation_RemoveItem()
+        public void Test_DataGridOperation_RemoveItem_Binding()
         {
             var window = new Window();
             var dataGrid = new DataGrid();
             window.Content = dataGrid;
             dataGrid.AutoGenerateColumns = false;
+
+            //DataGridにBindingをコードで指定
             var bindingSource = new BindingSorceTest();
             dataGrid.SetBinding(DataGrid.ItemsSourceProperty, new Binding(nameof(BindingSorceTest.Values)) { Source = bindingSource });
+
+            //削除ボタンのある列を作成、ボタンコントロールを直接作成するのではなく、そのFrameworkElementFactoryを作成する
             var removeButtonFactory = new FrameworkElementFactory(typeof(Button));
             removeButtonFactory.SetValue(DataGridOperation.RemoveItemProperty, true);
             removeButtonFactory.SetValue(Button.ContentProperty, "BUTTON");
-
             dataGrid.Columns.Add(new DataGridTemplateColumn()
             {
                 CellTemplate = new DataTemplate() { VisualTree = removeButtonFactory }
             });
 
+            //ウインドウ表示
             window.Show();
 
             bindingSource.Values
-                .Should().HaveCount(3);
+                .Should().HaveCount(3, "削除前は全行あるはず");
+            dataGrid.Items.Count
+                .Should().Be(3, "削除前は全行あるはず");
 
             var row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromIndex(1);
             var cell = dataGrid.Columns[0].GetCellContent(row);
             var child = VisualTreeHelper.GetChild(cell, 0);
+
+            //削除ボタン押下
             ((Button)child).RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
 
             bindingSource.Values
                 .Should().HaveCount(2, because: "1つ減ったはず");
             bindingSource.Values.Select(x => x.Value)
                 .Should().BeEquivalentTo(expectation: new[] { "A", "C" }, because: "1つ減ったはず");
+            dataGrid.Items.Count
+                .Should().Be(2, "1つ減ったはず");
+        }
 
+        [WpfFact]
+        public void Test_DataGridOperation_RemoveItem_NoBinding()
+        {
+            var window = new Window();
+            var dataGrid = new DataGrid();
+            window.Content = dataGrid;
+            dataGrid.AutoGenerateColumns = false;
+
+            //DataGridにBindingせず直接指定
+            var bindingSource = new BindingSorceTest();
+            dataGrid.ItemsSource = bindingSource.Values;
+
+            //削除ボタンのある列を作成、ボタンコントロールを直接作成するのではなく、そのFrameworkElementFactoryを作成する
+            var removeButtonFactory = new FrameworkElementFactory(typeof(Button));
+            removeButtonFactory.SetValue(DataGridOperation.RemoveItemProperty, true);
+            removeButtonFactory.SetValue(Button.ContentProperty, "BUTTON");
+            dataGrid.Columns.Add(new DataGridTemplateColumn()
+            {
+                CellTemplate = new DataTemplate() { VisualTree = removeButtonFactory }
+            });
+
+            //ウインドウ表示
+            window.Show();
+
+            bindingSource.Values
+                .Should().HaveCount(3, "削除前は全行あるはず");
+            dataGrid.Items.Count
+                .Should().Be(3, "削除前は全行あるはず");
+
+            var row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromIndex(1);
+            var cell = dataGrid.Columns[0].GetCellContent(row);
+            var child = VisualTreeHelper.GetChild(cell, 0);
+
+            //削除ボタン押下
+            ((Button)child).RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+
+            bindingSource.Values
+                .Should().HaveCount(2, because: "1つ減ったはず");
+            bindingSource.Values.Select(x => x.Value)
+                .Should().BeEquivalentTo(expectation: new[] { "A", "C" }, because: "1つ減ったはず");
+            dataGrid.Items.Count
+                .Should().Be(2, "1つ減ったはず");
         }
     }
 }
