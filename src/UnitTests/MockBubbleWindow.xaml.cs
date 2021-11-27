2a75f6122c3d6853fca986f8270596ddfbdfb293
soi013 <soi2flatline@gmail.com>
2021-11-27 17:29:49 +0900

    テストコードのリファクタリング

--- src/UnitTests/MockBubbleWindow.xaml.cs
+++ src/UnitTests/MockBubbleWindow.xaml.cs
@@ -1,29 +1,14 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace UnitTests;
+
 /// <summary>
 /// MockBubbleWindow.xaml の相互作用ロジック
 /// </summary>
 public partial class MockBubbleWindow : Window
 {
-    public Person[] Persons { get; set; } = new Person[]
-    {
-        new() { Name = "AAAA" } ,
-        new() { Name = "BBBB" } ,
-        new() { Name = "CCCC" } ,
-    };
-
     public MockBubbleWindow()
     {
         InitializeComponent();
     }
-}
-
-public class Person
-{
-    public string Name { get; set; } = "JANE DOE";
-
-    public int Age { get; set; } = 30;
 }
