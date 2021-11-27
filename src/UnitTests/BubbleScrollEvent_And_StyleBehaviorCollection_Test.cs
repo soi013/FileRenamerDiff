eb618e654774e7ef0f3028ad068b76e2a487e02f
soi013 <soi2flatline@gmail.com>
2021-11-27 11:46:45 +0900

    BubbleScrollEventtとStyleBehaviorCollectionのテストを追加

---
+++ src/UnitTests/BubbleScrollEvent_And_StyleBehaviorCollection_Test.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Input;
+
+namespace UnitTests;
+
+public class BubbleScrollEvent_And_StyleBehaviorCollection_Test
+{
+    [WpfFact]
+    public async Task BubbleScrollEvent_NoBubble()
+    {
+        MockBubbleWindow window = SetupWindow();
+
+        //ステージ1スクロール前
+        window.TargetScroll.VerticalOffset
+            .Should().Be(0, "スクロール前なので0のはず");
+
+        //ステージ2 Bublleしないスクロール後
+        var mouseWheelEventArgs1 = new MouseWheelEventArgs(Mouse.PrimaryDevice, Environment.TickCount, -500)
+        {
+            RoutedEvent = Mouse.PreviewMouseWheelEvent,
+        };
+        window.mockDataGridNotBubble.RaiseEvent(mouseWheelEventArgs1);
+        await Task.Delay(100);
+
+        window.TargetScroll.VerticalOffset
+            .Should().Be(0, "スクロールが伝わらないので0のはず");
+    }
+
+    [WpfFact]
+    public async Task BubbleScrollEvent_Bubble()
+    {
+        MockBubbleWindow window = SetupWindow();
+
+        //ステージ1スクロール前
+        window.TargetScroll.VerticalOffset
+            .Should().Be(0, "スクロール前なので0のはず");
+
+        //ステージ2 Bublleするスクロール後
+        var mouseWheelEventArgs1 = new MouseWheelEventArgs(Mouse.PrimaryDevice, Environment.TickCount, -500)
+        {
+            RoutedEvent = Mouse.PreviewMouseWheelEvent,
+        };
+        window.mockDataGridBubble.RaiseEvent(mouseWheelEventArgs1);
+        await Task.Delay(100);
+
+        window.TargetScroll.VerticalOffset
+            .Should().BeGreaterThan(1, "スクロールが伝わったので動いたはず");
+    }
+
+    [WpfFact]
+    public async Task BubbleScrollEvent_BubbleByStyleBehaviorCollection()
+    {
+        MockBubbleWindow window = SetupWindow();
+
+        //ステージ1スクロール前
+        window.TargetScroll.VerticalOffset
+            .Should().Be(0, "スクロール前なので0のはず");
+
+        //ステージ2 Bublleするスクロール後
+        var mouseWheelEventArgs1 = new MouseWheelEventArgs(Mouse.PrimaryDevice, Environment.TickCount, -500)
+        {
+            RoutedEvent = Mouse.PreviewMouseWheelEvent,
+        };
+        window.mockDataGridBubbleStyle.RaiseEvent(mouseWheelEventArgs1);
+        await Task.Delay(100);
+
+        window.TargetScroll.VerticalOffset
+            .Should().BeGreaterThan(1, "スクロールが伝わったので動いたはず");
+    }
+
+    private static MockBubbleWindow SetupWindow()
+    {
+        var window = new MockBubbleWindow()
+        {
+            Top = -10000,
+        };
+        //ウインドウ表示
+        window.Show();
+        return window;
+    }
+}
