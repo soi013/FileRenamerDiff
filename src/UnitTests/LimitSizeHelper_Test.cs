41f3af95a5f4c63ab09fa2f5f51f7cbddf21af08
soi013 <soi2flatline@gmail.com>
2021-11-30 15:07:18 +0900

    LimitSizeHelperのテスト結果が誤っていたバグを修正

--- src/UnitTests/LimitSizeHelper_Test.cs
+++ src/UnitTests/LimitSizeHelper_Test.cs
@@ -43,7 +43,7 @@ public class LimitSizeHelper_Test
             .Should().BeInRange(90, 110, "大きいコンテンツなので、サイズが制限ギリギリまで増えているはず");
 
         LimitSizeHelper.GetLimitMaxHeight(tb)
-            .Should().Be(0d);
+            .Should().Be(1d);
         LimitSizeHelper.GetLimitMaxWidth(tb)
             .Should().Be(0.5d);
     }
@@ -86,6 +86,6 @@ public class LimitSizeHelper_Test
         LimitSizeHelper.GetLimitMaxHeight(tb)
             .Should().Be(0.5d);
         LimitSizeHelper.GetLimitMaxWidth(tb)
-            .Should().Be(0d);
+            .Should().Be(-1d);
     }
 }
