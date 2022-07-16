c1a4911e808f8a2e6848eeb727dc2b89c394f43a
soi013 <soi2flatline@gmail.com>
2022-07-16 17:22:03 +0900

    ヘルプ作成PowerShellにPandocのパス探索を追加

--- src/FileRenamerDiff/HowToUse/CreateHtmlPandoc.ps1
+++ src/FileRenamerDiff/HowToUse/CreateHtmlPandoc.ps1
@@ -3,31 +3,50 @@
 cd 'HowToUse'
 $nameHeader = 'how_to_use'
 # 各言語ごとのコード
-$langCodes = @('','.de','.ru','.zh','.ja')
+$langCodes = @('', '.de', '.ru', '.zh', '.ja')
+
+$commandName = 'pandoc'
+
+$commandPathSpare1 = 'C:\Users\user\AppData\Local\Pandoc\pandoc.exe'
+$commandPathSpare2 = 'C:\Program Files\Pandoc\pandoc.exe'
+
+$commandPath = ''
+
+if (Get-Command $commandName -ea SilentlyContinue) {
+    $commandPath = $commandName
+}
+elseif (Test-Path $commandPathSpare1) {
+    $commandPath = $commandPathSpare1
+}
+elseif (Test-Path $commandPathSpare2) {
+    $commandPath = $commandPathSpare2
+}
+else {
+    echo "ERROR cannnot found command [$commandName]."
+    Exit 1
+}
+
+echo "pandoc path is $commandPath"
 
 # 各言語ごとに1つのHTMLファイルができる
-foreach($langCode in $langCodes)
-{
+foreach ($langCode in $langCodes) {
     $sourcePath = ".\$nameHeader$langCode.md"
     $sourceTime = $(Get-ItemProperty $sourcePath).LastWriteTime
     $targetPath = "..\Resources\$nameHeader$langCode.html"
-    if (Test-Path $targetPath)
-    {
+    if (Test-Path $targetPath) {
         $targetTime = $(Get-ItemProperty $targetPath).LastWriteTime
     }
-    else
-    {
+    else {
         $targetTime = 0
     }
 
     echo "Creation $nameHeader$langCode.html: sourceTime is $sourceTime, targetTime is $targetTime"
 
     # 生成したHTMLファイルが元のMarkdownファイルより更新日時が新しいときのみコンバートする
-    if ( $sourceTime -gt   $targetTime )
-    {
+    if ( $sourceTime -gt $targetTime ) {
         echo "Start Create $nameHeader$langCode.html"
         # Pandocを使用してMarkdownからHTMLファイルを生成する。cssなどを指定する
-        & 'C:\Program Files\Pandoc\pandoc' -s ./$nameHeader$langCode.md -o ../Resources/$nameHeader$langCode.html --toc --template=./elegant_bootstrap_menu.html --self-contained -t html5 -c my_markdown.css
+        & $commandPath -s ./$nameHeader$langCode.md -o ../Resources/$nameHeader$langCode.html --toc --template=./elegant_bootstrap_menu.html --self-contained -t html5 -c my_markdown.css
         echo "Finished Create $nameHeader$langCode.html"
     }
 } 
